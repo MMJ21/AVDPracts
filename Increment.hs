670e32d6c8c2746af1a4efd8cbdef854734c5833
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2021-09-27 20:40:49 +0200

    Update Increment.hs
    Ejercicios programación funcional.

--- Increment.hs
+++ Increment.hs
@@ -1,3 +1,14 @@
 module Increment where
 increment :: Int ->Int
 increment x = x + 1
+
+equals :: Int -> Int -> Bool
+equals x y = if x == y then True else False
+
+equalToZero :: Int -> Bool
+equalToZero 0 = True
+equalToZero _ = False
+
+equalToZeroTwo :: Int -> Bool
+equalToZeroTwo x | x == 0 = True
+                 | otherwise = False
