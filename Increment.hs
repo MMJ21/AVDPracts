6110043d7cde722ce70bd80c7e92ddcbc7e73718
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2021-09-27 21:18:59 +0200

    Update Increment.hs
    No funciona lista

--- Increment.hs
+++ Increment.hs
@@ -12,3 +12,9 @@ equalToZero _ = False
 equalToZeroTwo :: Int -> Bool
 equalToZeroTwo x | x == 0 = True
                  | otherwise = False
+
+findInList :: [Int] -> Int -> Int
+findInList (x:xs) y | (length (x:xs) == y) = findInList xs (y - 1)
+                | (length xs > y) = findInList xs y
+                | (y > length (x:xs)) = -1
+                | otherwise = x
