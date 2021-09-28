5bb1064fc70c33424d666347918266cdd48f78bc
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2021-09-28 11:24:33 +0200

    Update Increment.hs
    Ya funciona lista pero megafeo

--- Increment.hs
+++ Increment.hs
@@ -14,7 +14,9 @@ equalToZeroTwo x | x == 0 = True
                  | otherwise = False
 
 findInList :: [Int] -> Int -> Int
-findInList (x:xs) y | (length (x:xs) == y) = findInList xs (y - 1)
-                | (length xs > y) = findInList xs y
-                | (y > length (x:xs)) = -1
-                | otherwise = x
+findInList (x:xs) y | length(x:xs) < y = -1
+                    | y == 1 = x
+                    | y > 1 && not ((x:xs) == []) = findInList xs (y - 1)
+                    | otherwise = -1
+
+
