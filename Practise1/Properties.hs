386dd439c28eaf3185695ff7cc3c99967337309e
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2021-09-28 14:19:28 +0200

    Update Properties.hs
    Ejercicio 11

--- Practise1/Properties.hs
+++ Practise1/Properties.hs
@@ -26,7 +26,7 @@ prop_MaxLe :: Int -> Int -> Property
 prop_MaxLe x y = x <= y ==> max x y == y
 
 prop_Ordered :: Int -> [Int] -> Property
-prop_Ordered x xs = myOrdered xs ==> classify (null xs) "trivial" $ myOrdered (myInsert x xs)
+prop_Ordered x xs = myOrdered xs ==> collect (length xs) $ myOrdered (myInsert x xs)
 
 prop_DoubleCycle :: [Int] -> Int -> Property
 prop_DoubleCycle xs n = not (null xs) && n >= 0 ==>
