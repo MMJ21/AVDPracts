9fac8d506e34b47b65abbb8d27b5bab6d9e4a435
Marcos Martínez Játiva <marmarj2@inf.upv.es>
2021-09-28 14:04:47 +0200

    Update Properties.hs
    Ejercicio 7

--- Practise1/Properties.hs
+++ Practise1/Properties.hs
@@ -1,3 +1,5 @@
+import Test.QuickCheck
+
 prop_RevUnit :: Int -> Bool
 prop_RevUnit x = reverse [x] == [x]
 
@@ -9,3 +11,6 @@ prop_RevRev xs = reverse (reverse xs) == xs
 
 prop_RevApp_wrong :: [Int] -> [Int] -> Bool
 prop_RevApp_wrong xs ys = reverse (xs ++ ys) == reverse xs ++ reverse ys
+
+prop_MaxLe :: Int -> Int -> Property
+prop_MaxLe x y = x <= y ==> max x y == y
